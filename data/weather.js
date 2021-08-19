895a58efb11ac1e1b31ff8236e56c2c1a167739d
AHMADQURAAN97 <ahmadqouraan@gmail.com>
2021-08-19 22:47:13 +0300

    complet catch

--- data/weather.js
+++ data/weather.js
@@ -3,27 +3,37 @@ const axios = require("axios");
 
 
 
-
+let inMemory = {};
 
 
  async function getWeatherHandler (request,response){
 let searchQuery = request.query.searchQuery;
 let url = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&city=${searchQuery}`
 
-try {
-    axios.get(url).then((weatherResult)=> {
-    let weatherArray = weatherResult.data.data.map((item)=>{
+if (inMemory[searchQuery] !== undefined){
 
-        return new country (item);
+    console.log('cache hit - Weather');
+    response.send(inMemory[searchQuery])
+}else {
+    console.log("cache miss -weather");
+
+    try {
+        axios.get(url).then((weatherResult)=> {
+        let weatherArray = weatherResult.data.data.map((item)=>{
     
+            return new country (item);
+        
+        });
+        response.send(weatherArray)
     });
-    response.send(weatherArray)
-});
-} catch(error) {
-    console.log('error from axios',error)
-    response.send(error)
-
+    } catch(error) {
+        console.log('error from axios',error)
+        response.send(error)
+    
+    }
 }
+
+
 }
 
 class country  {
